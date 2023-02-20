ba3ab5ba6b1e493294822d90e00d2dd4bf6564ce
genilson <genilson.fernandes@teamsoft.com.br>
2023-02-20 11:07:17 -0300

    config chakra

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraBaseProvider } from "@chakra-ui/react";
+import theme from "@/styles/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraBaseProvider resetCSS theme={theme}>
+      <Component {...pageProps} />
+    </ChakraBaseProvider>
+  );
 }
