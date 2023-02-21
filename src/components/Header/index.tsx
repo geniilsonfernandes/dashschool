9f32d73289e414d677ca332cc6f325b363b91fad
genilson <genilson.fernandes@teamsoft.com.br>
2023-02-21 12:04:05 -0300

    create siderbar

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,86 @@
+import { Flex, Text, Icon, Input, HStack, Box, Avatar } from "@chakra-ui/react";
+import {
+  RiNotificationLine,
+  RiSearchLine,
+  RiUserAddLine
+} from "react-icons/ri";
+import React from "react";
+
+const Header = () => {
+  return (
+    <Flex
+      as="header"
+      width="100%"
+      height="20"
+      maxWidth={1480}
+      marginX="auto"
+      marginTop="4"
+      paddingX="6"
+      align="center"
+    >
+      <Text
+        fontSize={["2xl", "3xl"]}
+        fontWeight="bold"
+        letterSpacing="tight"
+        width="64"
+      >
+        dashgo
+        <Text as="span" color="pink.500" marginLeft="1">
+          .
+        </Text>
+      </Text>
+
+      <Flex
+        as="label"
+        flex="1"
+        paddingY="4"
+        paddingX="8"
+        marginLeft="6"
+        alignSelf="center"
+        align="center"
+        color="gray.200"
+        position="relative"
+        bg="gray.800"
+        borderRadius="full"
+        maxWidth={400}
+      >
+        <Input
+          color="gray.50"
+          variant="unstyled"
+          paddingX="4"
+          marginRight="4"
+          placeholder="Buscar na plataforma"
+          _placeholder={{ color: "gray.400" }}
+        />
+        <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+
+      <HStack
+        spacing={["6", "8"]}
+        marginX={["6", "8"]}
+        paddingRight={["6", "8"]}
+        paddingY="1"
+        color="gray.300"
+        borderRightWidth={1}
+        borderColor="gray.700"
+      >
+        <Icon as={RiNotificationLine} fontSize="20" />
+        <Icon as={RiUserAddLine} fontSize="20" />
+      </HStack>
+      <Flex align="center">
+        {true && (
+          <Box marginRight="4" textAlign="right">
+            <Text>genilson</Text>
+            <Text color="gray.300" fontSize="small">
+              ddfdfdfd
+            </Text>
+          </Box>
+        )}
+
+        <Avatar size="md" name="ds" src="" />
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Header;
---
+++ src/components/Siderbar/index.tsx
@@ -0,0 +1,33 @@
+import { Box, Link, Stack, Text, Icon } from "@chakra-ui/react";
+import React from "react";
+import { RiContactsLine, RiDashboardLine } from "react-icons/ri";
+
+const Siderbar = () => {
+  return (
+    <Box as="aside" w="64" mr="8">
+      <Stack spacing="12" align="flex-end">
+        <Box fontWeight="bold" color="gray.400" fontSize="small">
+          <Text fontWeight="bold" color="gray.400" fontSize="small">
+            Geral
+          </Text>
+          <Stack spacing="4" mt="8" align="stretch">
+            <Link display="flex" alignItems="center">
+              <Icon as={RiDashboardLine} />
+              <Text ml="4" fontWeight="medium">
+                Dashboard
+              </Text>
+            </Link>
+            <Link display="flex" alignItems="center">
+              <Icon as={RiContactsLine} />
+              <Text ml="4" fontWeight="medium">
+                usuários
+              </Text>
+            </Link>
+          </Stack>
+        </Box>
+      </Stack>
+    </Box>
+  );
+};
+
+export default Siderbar;
