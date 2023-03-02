db92577d633621a90ab3b9054340f3dd2efb40f0
genilson <genilson.fernandes@teamsoft.com.br>
2023-03-02 08:23:11 -0300

    feat: criado o input da aplicação e seu teste

--- src/components/Form/Input/index.tsx
+++ src/components/Form/Input/index.tsx
@@ -2,29 +2,45 @@ import {
   Input as InputChakra,
   FormControl,
   FormLabel,
-  InputProps
+  InputProps,
+  FormHelperText,
+  InputGroup,
+  InputLeftAddon
 } from "@chakra-ui/react";
 
 type Props = {
   name: string;
   label: string;
+  helperText?: string;
 } & InputProps;
 
-const Input = ({ name, type, label }: Props) => {
+const Input = ({
+  name,
+  type,
+  label,
+  placeholder,
+  helperText,
+  ...props
+}: Props) => {
   return (
     <FormControl>
       <FormLabel htmlFor={name}>{label}</FormLabel>
-      <InputChakra
-        id={name}
-        name={name}
-        type={type}
-        focusBorderColor="pink.500"
-        bgColor="gray.900"
-        variant={"filled"}
-        _hover={{
-          bgColor: "gray.900"
-        }}
-      />
+      <InputGroup>
+        <InputChakra
+          id={name}
+          name={name}
+          type={type}
+          focusBorderColor="pink.500"
+          bgColor="gray.900"
+          variant={"filled"}
+          placeholder={placeholder}
+          _hover={{
+            bgColor: "gray.900"
+          }}
+          {...props}
+        />
+      </InputGroup>
+      <FormHelperText>{helperText}</FormHelperText>
     </FormControl>
   );
 };
--- src/components/Form/Input/input.test.tsx
+++ src/components/Form/Input/input.test.tsx
@@ -1,17 +1,48 @@
 import React from "react";
+import userEvent from "@testing-library/user-event";
 import { render, screen } from "@testing-library/react";
 import { Input } from ".";
 
 describe("Input", () => {
-  it("Input should render", () => {
+  it("should have label bar and name foo", () => {
     render(<Input name="foo" label="bar" />);
     const input = document.querySelector("input");
 
-    screen.debug();
-
     const label = screen.getByLabelText("bar");
-
     expect(label).toBeInTheDocument();
     expect(input).toHaveAttribute("name", "foo");
+  });
+  it("should have type password", () => {
+    render(<Input name="foo" label="bar" type="password" />);
+    const input = document.querySelector("input");
+    expect(input).toHaveAttribute("type", "password");
+  });
+  it("should have write foo in input", () => {
+    render(<Input name="foo" label="bar" placeholder="foo" />);
+
+    const input = screen.getByPlaceholderText("foo");
+    expect(input).toBeInTheDocument();
+
+    userEvent.type(input, "foo");
+
+    expect(input).toHaveValue("foo");
+  });
+  it("should have helper text", () => {
+    render(<Input name="foo" label="bar" helperText="helper text" />);
+
+    const helperText = screen.getByText("helper text");
+    expect(helperText).toBeInTheDocument();
+  });
+  it("should have focus when click in label and", () => {
+    render(<Input name="foo" label="bar" />);
+
+    const input = document.querySelector("input");
+    const label = screen.getByLabelText("bar");
+
+    expect(input).not.toHaveFocus();
+
+    userEvent.click(label);
+
+    expect(input).toHaveFocus();
   });
 });
