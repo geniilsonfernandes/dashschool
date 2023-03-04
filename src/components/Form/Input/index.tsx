b67e68a8196d3a66683241c3033ba84d803cb237
genilson <genilson.fernandes@teamsoft.com.br>
2023-03-04 18:02:03 -0300

    feat: modificado componente de input para ter o tipo password
    Changes to be committed:
    	modified:   src/components/Form/Input/index.tsx

--- src/components/Form/Input/index.tsx
+++ src/components/Form/Input/index.tsx
@@ -1,17 +1,22 @@
 import {
-  Input as InputChakra,
+  Button,
   FormControl,
-  FormLabel,
-  InputProps,
   FormHelperText,
+  FormLabel,
+  Input as InputChakra,
   InputGroup,
-  InputLeftAddon
+  InputProps,
+  InputRightElement
 } from "@chakra-ui/react";
+import { useState } from "react";
+
+import { RiEyeFill, RiEyeCloseFill } from "react-icons/ri";
 
 type Props = {
   name: string;
   label: string;
   helperText?: string;
+  error?: boolean;
 } & InputProps;
 
 const Input = ({
@@ -20,27 +25,55 @@ const Input = ({
   label,
   placeholder,
   helperText,
+  error,
   ...props
 }: Props) => {
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
+
   return (
     <FormControl>
-      <FormLabel htmlFor={name}>{label}</FormLabel>
+      <FormLabel htmlFor={name} color={error ? "red.500" : "white"}>
+        {label}
+      </FormLabel>
       <InputGroup>
         <InputChakra
           id={name}
           name={name}
-          type={type}
-          focusBorderColor="pink.500"
+          type={
+            type === "password" && show
+              ? "text"
+              : type === "password"
+              ? "password"
+              : type
+          }
+          borderColor={error ? "red.500" : "gray.900"}
+          focusBorderColor={error ? "red.500" : "pink.500"}
           bgColor="gray.900"
           variant={"filled"}
           placeholder={placeholder}
           _hover={{
             bgColor: "gray.900"
           }}
+          paddingRight={type === "password" ? "4.5rem" : "0"}
           {...props}
         />
+        {type === "password" && (
+          <InputRightElement mr="0.5rem">
+            <Button
+              h="1.75rem"
+              size="sm"
+              onClick={handleClick}
+              colorScheme="whiteAlpha"
+            >
+              {show ? <RiEyeFill /> : <RiEyeCloseFill />}
+            </Button>
+          </InputRightElement>
+        )}
       </InputGroup>
-      <FormHelperText>{helperText}</FormHelperText>
+      <FormHelperText color={error ? "red.500" : "white"}>
+        {helperText}
+      </FormHelperText>
     </FormControl>
   );
 };
