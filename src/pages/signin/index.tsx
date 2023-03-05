937f5b340982216caef33adc12abea6fad6fd03c
genilson <genilson.fernandes@teamsoft.com.br>
2023-03-05 10:36:49 -0300

    feat: create form to create user
    Changes to be committed:
    	modified:   src/pages/signin/index.tsx

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -2,65 +2,71 @@ import Button from "@/components/Button";
 import { Input } from "@/components/Form/Input";
 import { stylesConstants } from "@/styles";
 import {
-  Flex,
-  Stack,
-  Heading,
-  Link as ChakraLink,
+  Box,
   Divider,
-  Center,
+  Flex,
+  Heading,
   HStack,
+  Link as ChakraLink,
   PinInput,
   PinInputField,
-  Text,
-  useDisclosure,
   SlideFade,
-  Box
+  Stack,
+  Text,
+  useDisclosure
 } from "@chakra-ui/react";
 import Link from "next/link";
 
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Controller, useForm } from "react-hook-form";
+import * as yup from "yup";
+import OtpVerificate from "@/components/OtpVerificate";
+
+const schema = yup.object().shape({
+  email: yup.string().required("Campo obrigatório").email("Email inválido"),
+  password: yup.string().required("Campo obrigatório"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password") ?? null], "As senhas precisam ser iguais")
+    .required("Campo obrigatório")
+});
+
+export type IFormCreateValues = {
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
 export default function SignIn() {
-  const { isOpen, onToggle } = useDisclosure();
+  const { isOpen, onToggle, onOpen } = useDisclosure();
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isSubmitting }
+  } = useForm<IFormCreateValues>({
+    mode: "onSubmit",
+    resolver: yupResolver(schema)
+  });
 
   const handleVerify = (otpCode: any) => {
     console.log(otpCode);
+  };
+
+  const handleCreateUser = (values: IFormCreateValues) => {
+    console.log(values);
+    onOpen();
   };
 
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
       <Box position={"relative"}>
         <SlideFade in={isOpen} offsetY="20px">
-          <Flex
-            w={["310px", "360px"]}
-            maxWidth="360px"
-            bg="gray.800"
-            p="8"
-            borderRadius={8}
-            flexDir="column"
-            position="absolute"
-            zIndex={10}
-          >
-            <Heading>Verificação</Heading>
-            <Divider borderColor="gray.600" my="6" />
-            <Text align="center" mb="8">
-              Digite o código de verificação enviado para seu e-mail
-            </Text>
-            <HStack justifyContent="center" spacing={5}>
-              <PinInput onChange={handleVerify}>
-                <PinInputField />
-                <PinInputField />
-                <PinInputField />
-                <PinInputField />
-              </PinInput>
-            </HStack>
-            <Button mt="8">Verificar agora</Button>
-            <Button mt="2" variant="outline" onClick={onToggle}>
-              Voltar
-            </Button>
-          </Flex>
+          <OtpVerificate />
         </SlideFade>
 
         <Flex
-          w="360px"
+          w={["310px", "360px"]}
           maxWidth={360}
           bg="gray.800"
           p="8"
@@ -74,16 +80,57 @@ export default function SignIn() {
           <Divider borderColor="gray.600" my="6" />
           <Flex as="form" borderRadius={8} flexDir="column">
             <Stack spacing={4}>
-              <Input label="Email" name="email" type="email" />
-              <Input label="Senha" name="password" type="password" />
-              <Input
-                label="Confirma senha"
-                name="password_confirmation"
-                type="password"
+              <Controller
+                name="email"
+                control={control}
+                render={({ field: { value, onChange } }) => (
+                  <Input
+                    name="email"
+                    label="Email"
+                    value={value}
+                    onChange={onChange}
+                    error={errors.email ? true : false}
+                    helperText={errors.email?.message}
+                  />
+                )}
+              />
+              <Controller
+                name="password"
+                control={control}
+                render={({ field: { value, onChange } }) => (
+                  <Input
+                    name="password"
+                    type="password"
+                    label="Senha"
+                    value={value}
+                    onChange={onChange}
+                    error={errors.password ? true : false}
+                    helperText={errors.password?.message}
+                  />
+                )}
+              />
+              <Controller
+                name="confirmPassword"
+                control={control}
+                render={({ field: { value, onChange } }) => (
+                  <Input
+                    name="password_confirmation"
+                    type="password"
+                    label="Senha"
+                    value={value}
+                    onChange={onChange}
+                    error={errors.confirmPassword ? true : false}
+                    helperText={errors.confirmPassword?.message}
+                  />
+                )}
               />
             </Stack>
-            <Button mt="6" onClick={onToggle}>
-              Criar conta
+            <Button
+              mt="6"
+              isLoading={isSubmitting}
+              onClick={handleSubmit(handleCreateUser)}
+            >
+              {isSubmitting ? "Criando conta..." : "Criar conta"}
             </Button>
           </Flex>
           <Button mt="2" variant="outline" onClick={onToggle}>
