50a15646caed824c9215fd438bfdd3fd07cf3810
genilson <genilson.fernandes@teamsoft.com.br>
2023-03-05 10:35:04 -0300

    feat: create otp verificate component
    Changes to be committed:
    	new file:   src/components/OtpVerificate/index.tsx

---
+++ src/components/OtpVerificate/index.tsx
@@ -0,0 +1,46 @@
+import {
+  Divider,
+  Flex,
+  Heading,
+  HStack,
+  PinInput,
+  PinInputField,
+  Text
+} from "@chakra-ui/react";
+import React from "react";
+import Button from "../Button";
+
+const OtpVerificate = () => {
+  return (
+    <Flex
+      w={["310px", "360px"]}
+      maxWidth="360px"
+      bg="gray.800"
+      p="8"
+      borderRadius={8}
+      flexDir="column"
+      position="absolute"
+      zIndex={10}
+    >
+      <Heading>Verificação</Heading>
+      <Divider borderColor="gray.600" my="6" />
+      <Text align="center" mb="8">
+        Digite o código de verificação enviado para seu e-mail
+      </Text>
+      <HStack justifyContent="center" spacing={5}>
+        <PinInput>
+          <PinInputField />
+          <PinInputField />
+          <PinInputField />
+          <PinInputField />
+        </PinInput>
+      </HStack>
+      <Button mt="8">Verificar agora</Button>
+      <Button mt="2" variant="outline">
+        Voltar
+      </Button>
+    </Flex>
+  );
+};
+
+export default OtpVerificate;
