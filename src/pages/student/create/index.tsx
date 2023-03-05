6d63b2175dffc7b894621806bf0daf60d9a74c46
genilson <genilson.fernandes@teamsoft.com.br>
2023-03-04 22:36:23 -0300

    feat: criado requisição de criar estudante

--- src/pages/student/create/index.tsx
+++ src/pages/student/create/index.tsx
@@ -1,14 +1,26 @@
+import useAsync from "@/hook/useAsync";
 import Base from "@/templates/Base";
+import axios from "axios";
 import Form, { IFormValues } from "../../../components/Form/FormCreateUser";
 
 const Create = () => {
-  const handleCreateUser = (values: IFormValues) => {
-    console.log(values);
+  const handleCreateUser = async (values: IFormValues) => {
+    try {
+      await axios.post("http://localhost:3000/api/student/create", {
+        name: values.name,
+        email: values.email,
+        password: values.password
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
+
+  const { isLoading, execute } = useAsync(handleCreateUser);
 
   return (
     <Base>
-      <Form onSubmit={handleCreateUser} />
+      <Form onSubmit={execute} isLoading={isLoading} />
     </Base>
   );
 };
