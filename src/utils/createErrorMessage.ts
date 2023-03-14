7f7b75d887a86acd8fc13c7e2b225af36d32cb2e
genilson <genilson.fernandes@teamsoft.com.br>
2023-03-14 18:05:35 -0300

    Changes to be committed: 	modified:   src/utils/createErrorMessage.ts

--- src/utils/createErrorMessage.ts
+++ src/utils/createErrorMessage.ts
@@ -3,8 +3,7 @@ import { AxiosError } from "axios";
 export function createErrorMessage(err: AxiosError) {
   let errorMessage = "Serviço indisponivel, tente novamente mais tarde!";
   if (err.response) {
-    const { data } = err.response;
-    errorMessage = data.message || errorMessage;
+    errorMessage = "Erro: ";
   }
   return errorMessage;
 }
